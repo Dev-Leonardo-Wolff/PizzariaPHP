687d073569a9f0526e3412c9c7a585011a8fb6aa
Leonardo Wolff <120134614+Dev-Leonardo-Wolff@users.noreply.github.com>
2022-12-09 10:28:51 -0300

    Add files via upload

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-define("DIR_PATH", "C:/xampp\htdocs\pizzariadojoao");
-define("URL_BASE","http://localhost/pizzariadojoao/");
+define("DIR_PATH", "C:/xampp\htdocs\PizzariaPHP");
+define("URL_BASE","http://localhost/PizzariaPHP/");
 define("TEMPLATE_BASE",DIR_PATH."/templates");
 define("URL_TEMPLATE",URL_BASE."/templates");
 ?>
